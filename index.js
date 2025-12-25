b77d8e9a7f6442c8c5975a46a15a0fa19a32a97f
phuhnf8 <phuhnf8@fullstack.edu.vn>
2025-12-25 21:24:35 +0700

    docs: update README with installation instructions and project details

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 
 const useRoute = require('@routing/index.js');
 const importHeaders = require('@head');
+
 const PORT = 3000;
 
 importHeaders(app);
 useRoute(app);
-// handleErrors(app);
 
 app.listen(PORT, () => {
     console.log('Server backend is running in port', PORT);
--- readme.md
+++ readme.md
@@ -1,1 +1,17 @@
-# Bai tap F8 - NodeJS - Buoi 2
+# ExpressJS Day 02
+
+Installation:
+
+```
+yarn
+```
+
+-   In default, server is run on port 3000. No front-end setup for this project.
+
+Run the project:
+
+```
+yarn start
+```
+
+Live demo: [here](https://f8-express-day02.onrender.com)
